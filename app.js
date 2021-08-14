4d600235c28a4956a8679a63e2721b3191c17aee
mdrasel2020 <73265916+mdrasel2020@users.noreply.github.com>
2021-08-15 02:46:07 +0300

    value emply

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function typeing() {
     let mess = document.getElementById('mess').value;
     let sfff = mess.match(/\w+/g);// regular expresoin javascript
     document.querySelector('span').innerText = 'The Total word : '+sfff.length;
+    document.getElementById('mess').value = ' ';
   
 }
  
